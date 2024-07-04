f09173182ee405341f0ee1c155ec2a4242cf0d36
George Roufaeil <it2021089@hua.gr>
2024-07-04 03:16:34 +0300

    test

--- localdocwebapp-vue/src/views/PendingDoctorView.vue
+++ localdocwebapp-vue/src/views/PendingDoctorView.vue
@@ -60,6 +60,8 @@ const doctorId =  router.currentRoute.value.query.doctorId
       const showModal = ref(false);
       const msg = ref('');
       const backendURL = import.meta.env.VITE_APP_API_URL;
+      console.log('backendURL:', backendURL); // Προσθέστε αυτό το log
+
       const acceptClient = (clientId) => { //If the doctors clicks on Accept Client send a POST request to accept him
 
   fetch(`${backendURL}/api/pending/show/${doctorId}/${clientId}`, {
